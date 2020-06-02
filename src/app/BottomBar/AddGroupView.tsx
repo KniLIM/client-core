4a1f539598d81685ceb14b605bed54f1ce3cab35
Faymine <348607968@qq.com>
2020-06-02 15:39:54 +0800

    feat:添加好友确认&验证信息,搜索/加入群聊

--- src/app/BottomBar/AddGroupView.tsx
+++ src/app/BottomBar/AddGroupView.tsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import React from 'react'
+import {useState} from 'react'
+import { Input, Spin, Avatar } from 'antd';
+import { List, Skeleton } from 'antd'
+import GroupItem from './groupItem'
+
+const { Search } = Input
 
 export default () => {
+    const [loading, setLoading] = useState(false)
+    const [groupList, setGroupList] = useState<any[]>([])
+    const [current, setCurrent] = useState(1)
+
+    // 测试数据
+    const groupTmp = ["飞电智能公司", "哉亚集团", "A.I.M.S", "卫星泽亚", "卫星亚克",
+    "飞电智能公司", "哉亚集团", "A.I.M.S", "卫星泽亚", "卫星亚克",
+    "飞电智能公司", "哉亚集团", "A.I.M.S", "卫星泽亚", "卫星亚克",
+    "飞电智能公司", "哉亚集团", "A.I.M.S", "卫星泽亚", "卫星亚克",
+    "飞电智能公司", "哉亚集团", "A.I.M.S"]
+
+    const avatar = "https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+
+    const searchFriend = (keyword : any) => {
+        setLoading(true)
+        setGroupList(groupTmp)
+        setLoading(false)
+    }
+
     return (
-        <p>some content...</p>
+        <div>
+            <Search 
+            placeholder = "搜索群聊"
+            onSearch = {value => searchFriend(value)}
+            style = {{ width:"100%" }}
+            />
+            <div>
+                <Spin 
+                spinning={loading} 
+                style = {{ width:"100%" }}>
+                </Spin>
+                <List
+                className='friend-search-result'
+                itemLayout='horizontal'
+                locale={{emptyText:"赶快搜索并加入群聊吧~"}}
+                dataSource={groupList}
+                renderItem={friend =>(
+                    <Skeleton avatar title = {false} loading={loading} active>
+                        <GroupItem
+                            id="1234"
+                            name={friend}
+                            avatar={avatar}
+                            loading={loading}
+                        /> 
+                    </Skeleton>
+                )}
+                pagination={{
+                    current: current,
+                    defaultPageSize: 10,
+                    size: 'small',
+                    total: groupList.length,
+                    hideOnSinglePage: true,
+                    onChange: (page) => setCurrent(page),
+                    position: 'bottom'
+                }}
+                />
+            </div>
+        </div>
     )
-};
+}
