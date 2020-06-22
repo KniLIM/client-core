3c3df4bf46dca5f8f469b8d3c82715ea9fbdef67
Zx55 <czr.cn.525@gmail.com>
2020-06-22 13:37:07 +0800

    fix: addMsg

--- src/app/ChatBox/service/index.tsx
+++ src/app/ChatBox/service/index.tsx
@@ -1,7 +1,7 @@
-import {useState, useEffect} from 'react';
-import {createModel} from 'hox';
-import {message} from 'antd';
-import {getDB} from 'utils';
+import { useState, useEffect } from 'react';
+import { createModel } from 'hox';
+import { message } from 'antd';
+import { getDB } from 'utils';
 import useService, { TABS } from 'app/Service';
 
 export interface IMsgRecord {
@@ -32,15 +32,15 @@ const initMsgList = async (): Promise<IMsgList> => {
     if (!db) return {};
 
     return new Promise((resolve, reject) => {
-        const a:Array<IMsgRecord> = [];
-        const b:Array<IMsgRecord> = [];
+        const a: Array<IMsgRecord> = [];
+        const b: Array<IMsgRecord> = [];
         let msgList: IMsgList = {};
         const msgListStore = db.transaction('msgList', 'readonly').objectStore('msgList');
         const getRequest = msgListStore.getAll();
 
         getRequest.onsuccess = (e: any) => {
             const msgs = e.target.result as Array<{ id: string, msgs: Array<IMsgRecord> }>;
-            msgs.forEach((msg) => msgList[msg.id.toString()] = { msgs: msg.msgs});
+            msgs.forEach((msg) => msgList[msg.id.toString()] = { msgs: msg.msgs });
             resolve(msgList);
         }
 
@@ -53,8 +53,8 @@ const initMsgList = async (): Promise<IMsgList> => {
 export default createModel(() => {
     const [msgList, setMsgList] = useState<IMsgList>({});
     const [sortedMsgList, setSortedMsgList] = useState<Array<string>>([]);
-    const {setChatBoxId} = useService();
-    const {setTabBar} = useService();
+    const { setChatBoxId } = useService();
+    const { setTabBar } = useService();
 
     useEffect(() => {
         initMsgList().then(res => {
@@ -62,19 +62,23 @@ export default createModel(() => {
             const sortedIdList = Object.keys(res).sort((a, b) => {
                 const msg1 = res[a]['msgs'];
                 const msg2 = res[b]['msgs'];
-                const date1 = msg1[msg1.length-1].date;
-                const date2 = msg2[msg2.length-1].date;
-                if(date1 < date2) return 1;
-                else if(date1 === date2) return 0;
+                const date1 = msg1[msg1.length - 1].date;
+                const date2 = msg2[msg2.length - 1].date;
+                if (date1 < date2) return 1;
+                else if (date1 === date2) return 0;
                 else return -1;
             });
             setSortedMsgList(sortedIdList);
         });
     }, []);
 
+    // useEffect(() => {
+    //     console.log('msglist: ',sortedMsgList);
+    // }, [sortedMsgList])
+
     const createChat = (id: string) => {
         const index = sortedMsgList.indexOf(id);
-        if(index !== -1) sortedMsgList.splice(index, 1);
+        if (index !== -1) sortedMsgList.splice(index, 1);
         sortedMsgList.unshift(id);
         setChatBoxId(id);
         setTabBar(TABS.CHAT);
@@ -82,55 +86,51 @@ export default createModel(() => {
 
     const addMsg = (id: string, msg: IMsgRecord) => {
         const index = sortedMsgList.indexOf(id);
-        if(index !== -1) sortedMsgList.splice(index, 1);
+        if (index !== -1) sortedMsgList.splice(index, 1);
         sortedMsgList.unshift(id);
+
         getDB().then(db => {
             if (db) {
                 const msgListStore = db.transaction('msgList', 'readwrite').objectStore('msgList');
-                let addMsgRequest: IDBRequest<IDBValidKey>;
-
-                if (id in msgList) {
-                    let msgs = msgList[id].msgs;
-                    msgs.push(msg);
-
-                    addMsgRequest = msgListStore.put({ id, msgs });
-                    addMsgRequest.onsuccess = ((e: Event) => {
-                        setMsgList(prev => ({
-                            ...prev,
-                            [id]: {msgs},
-                        }));
-                    });
-                } else {
-                    addMsgRequest = msgListStore.add({ id, msgs: [msg] });
-                    addMsgRequest.onsuccess = ((e: Event) => {
-                        setMsgList(prev => ({
-                            ...prev,
-                            [id]: { msgs: [msg] },
-                        }));
-                    });
-                }
-
-                addMsgRequest.onerror = ((e: Event) => {
-                    message.error('数据更新失败');
+                const getRequest = msgListStore.get(id);
+                getRequest.onsuccess = ((e: any) => {
+                    if (typeof (e.target.result) === 'undefined') {
+                        let newList = [msg];
+                        let addOrPutRequest = msgListStore.add({ id, msgs: newList });
+                        addOrPutRequest.onsuccess = ((e: Event) => {
+                            setMsgList(prev => ({
+                                ...prev,
+                                [id]: { msgs: newList },
+                            }));
+                        });
+                        addOrPutRequest.onerror = ((e: any) => {
+                            message.error('数据库更新失败');
+                            console.log(e);
+                        });
+                    } else {
+                        let newList = e.target.result.msgs as Array<IMsgRecord>;
+                        newList.push(msg);
+                        let addOrPutRequest = msgListStore.put({ id, msgs: newList });
+                        addOrPutRequest.onsuccess = ((e: Event) => {
+                            setMsgList(prev => ({
+                                ...prev,
+                                [id]: { msgs: newList },
+                            }));
+                        });
+                        addOrPutRequest.onerror = ((e: any) => {
+                            message.error('数据库更新失败');
+                            console.log(e);
+                        });
+                    }
                 });
-            } else {
-                if (id in msgList) {
-                    let msgs = msgList[id].msgs;
-                    msgs.push(msg);
 
-                    setMsgList(prev => ({
-                        ...prev,
-                        [id]: {msgs},
-                    }));
-                } else {
-                    setMsgList(prev => ({
-                        ...prev,
-                        [id]: { msgs: [msg] },
-                    }));
-                }
+                getRequest.onerror = ((e: Event) => {
+                    message.error('数据库读取失败');
+                    console.log(e);
+                });
             }
         });
     }
 
-    return {msgList, setMsgList,addMsg, sortedMsgList, setSortedMsgList, createChat};
+    return { msgList, setMsgList, addMsg, sortedMsgList, setSortedMsgList, createChat };
 });
